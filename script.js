2965408aacbe31499486d7baf3f436a3123972ff
Samuel Constantino <samuelicapuidados@gmail.com>
2021-05-05 16:08:33 -0300

    implementa requisito 2: adiciona paleta de cores

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    // adiciona cores em palette
+    function addColorsInPalette() {
+        let elementColors = document.querySelectorAll('.color');
+        let colorsList = ['black', 'blue', 'red', 'green'];
+        for(let key in colorsList){
+            elementColors[key].style.backgroundColor = colorsList[key];
+        }
+    }
+    addColorsInPalette();
+}
