581cc1322f42534c61dc6576e8c8778584784679
Samuel Constantino <samuelicapuidados@gmail.com>
2021-05-05 16:38:31 -0300

    implementa requisito 4: adiciona quadro de pixels

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   // adiciona cores em palette
   function addColorsInPalette() {
-    let elementColors = document.querySelectorAll('.color');
+    const elementColors = document.querySelectorAll('.color');
     let colorsList = ['black', 'blue', 'red', 'green'];
     for (let key in colorsList) {
       elementColors[key].style.backgroundColor = colorsList[key];
